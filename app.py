c37cb0a3ead1cac559584f2d75dfb123b3186c16
Wasaka BegeinTv <127060500+WasakaBe@users.noreply.github.com>
2024-06-06 14:44:11 -0600

    Add files via upload

--- app.py
+++ app.py
@@ -1,8 +1,47 @@
+<<<<<<< HEAD
+from flask import Flask, request, render_template, jsonify
+import joblib
+import pandas as pd
+import logging
+=======
 from flask import Flask, request, send_from_directory
+>>>>>>> 62294206eebfad6df56ab969ee1e4f132f66e63d
 
 app = Flask(__name__)
 
+# Configurar el registro
+logging.basicConfig(level=logging.DEBUG)
+
+# Cargar el modelo entrenado
+model = joblib.load('modelo.pkl')
+app.logger.debug('Modelo cargado correctamente.')
+
 @app.route('/')
+<<<<<<< HEAD
+def home():
+    return render_template('formulario.html')
+
+@app.route('/predict', methods=['POST'])
+def predict():
+    try:
+        # Obtener los datos enviados en el request
+        abdomen = float(request.form['abdomen'])
+        antena = float(request.form['antena'])
+        
+        # Crear un DataFrame con los datos
+        data_df = pd.DataFrame([[abdomen, antena]], columns=['abdomen', 'antena'])
+        app.logger.debug(f'DataFrame creado: {data_df}')
+        
+        # Realizar predicciones
+        prediction = model.predict(data_df)
+        app.logger.debug(f'Predicción: {prediction[0]}')
+        
+        # Devolver las predicciones como respuesta JSON
+        return jsonify({'categoria': prediction[0]})
+    except Exception as e:
+        app.logger.error(f'Error en la predicción: {str(e)}')
+        return jsonify({'error': str(e)}), 400
+=======
 def index():
     return '''
     <!DOCTYPE html>
@@ -227,6 +266,7 @@ def index():
     </body>
     </html>
     '''
+>>>>>>> 62294206eebfad6df56ab969ee1e4f132f66e63d
 
 @app.route('/static/<path:path>')
 def send_static(path):
